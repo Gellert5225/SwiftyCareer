26f4a7a830f0bd7d1775633738bb84db3647ca80
gellert5225 <gellert.li1995@gmail.com>
2020-11-21 07:06:00 -0800

    added segmented control dependency

---
+++ iOS Client/SwiftyCareer/SwiftyCareer/UI/Discover/UserCell.swift
@@ -0,0 +1,23 @@
+//
+//  UserCell.swift
+//  SwiftyCareer
+//
+//  Created by Gellert Li on 11/21/20.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}

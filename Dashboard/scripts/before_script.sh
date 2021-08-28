87605d72acb6d49946f669d1881fe63fbd36eb61
Gellert5225 <gellert.li1995@gmail.com>
2021-08-27 17:26:32 -0700

    removed dashboard

--- Dashboard/scripts/before_script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-set -x
-
-test -f Parse-Dashboard/public/bundles/dashboard.bundle.js
-test -f Parse-Dashboard/public/bundles/login.bundle.js
-test -f Parse-Dashboard/public/bundles/sprites.svg
-test -d Parse-Dashboard/public/bundles/img
--- Dashboard/src/components/Sidebar/AppName.react.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from 'components/Sidebar/Sidebar.scss';
-
-export default ({ name, onClick }) => (
-  <div className={styles.currentApp} onClick={onClick}>
-    {name}
-  </div>
-);
